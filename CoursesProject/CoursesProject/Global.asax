41966f7e291dc91d833fe9c96597dd6f05bcd5a8
mohamed afifi <mohammedafifi153@gmail.com>
2020-09-24 08:24:34 +0200

    change

--- CoursesProject/CoursesProject/Global.asax
+++ CoursesProject/CoursesProject/Global.asax
@@ -1,1 +1,1 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoursesProject.MvcApplication" Language="C#" %>
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoursesProject.Global" Language="C#" %>
--- CoursesProject/CoursesProject/Global.asax.cs
+++ CoursesProject/CoursesProject/Global.asax.cs
@@ -1,18 +1,23 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
 
 namespace CoursesProject
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class Global : HttpApplication
     {
-        protected void Application_Start()
+        void Application_Start(object sender, EventArgs e)
         {
+            // Code that runs on application startup
             AreaRegistration.RegisterAllAreas();
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);            
         }
     }
 }
